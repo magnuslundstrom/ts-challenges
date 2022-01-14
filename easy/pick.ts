57976399b2acbe086640450fe76f4b7543fea17f
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2022-01-14 19:01:14 +0100

    Create pick.ts

---
+++ easy/pick.ts
@@ -0,0 +1,16 @@
+type MyPick<T, K extends keyof T> = {
+    [N in K]: T[N];
+};
+
+interface Todo {
+    title: string;
+    description: string;
+    completed: boolean;
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>;
+
+const todo: TodoPreview = {
+    title: 'Clean room',
+    completed: false,
+};
